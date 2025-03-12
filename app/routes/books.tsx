c5fb5fef65cccfc265801ff657f3ccdacf3390b1
Bartek <b.choncel@gmail.com>
2025-03-12 20:59:44 +0100

    added page books

--- app/routes/books.tsx
+++ app/routes/books.tsx
@@ -2,15 +2,15 @@ import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "Bartłomiej Choncel" },
+    { title: "Bartłomiej Choncel - books" },
     { name: "description", content: "Welcome to my website!" },
   ];
 }
 
-export default function Home() {
+export default function Books() {
   return (
     <div>
-      <h1>Hey, I'm Bartek</h1>
+      <h1>Books</h1>
     </div>
   );
 }
